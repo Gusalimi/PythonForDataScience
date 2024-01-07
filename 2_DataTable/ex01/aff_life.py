01928a63e1ea24ad40130cc540af0b52f21e9304
Guillaume Sailé <saileguillaume2@gmail.com>
2024-01-07 19:42:59 +0100

    [M02][ex01] Done

---
+++ 2_DataTable/ex01/aff_life.py
@@ -0,0 +1,23 @@
+from load_csv import load
+import matplotlib.pyplot as plt
+
+
+def main():
+    data = load("life_expectancy_years.csv")
+    if (data is None):
+        return
+    france = data.loc[data['country'] == "France"].iloc[:, 1:]
+
+    plt.plot(france.columns, france.values[0])
+
+    plt.title("France Life expectancy Projections")
+
+    plt.ylabel("Life expectancy")
+    plt.xlabel("Year")
+    plt.xticks(france.columns[::40])
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
