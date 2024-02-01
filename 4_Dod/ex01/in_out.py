2e9537d6a36e07208325a172828a4bbafd1c3721
Guillaume Sailé <saileguillaume2@gmail.com>
2024-02-01 17:23:57 +0100

    [M04][ex01] Docstring

--- 4_Dod/ex01/in_out.py
+++ 4_Dod/ex01/in_out.py
@@ -1,15 +1,19 @@
 def square(x: int | float) -> int | float:
+    """Return the square of x."""
     return (x * x)
 
 
 def pow(x: int | float) -> int | float:
+    """Return x to the power of x."""
     return (x ** x)
 
 
 def outer(x: int | float, function) -> object:
+    """Return a function that will apply function to x n times."""
     count = 0
 
     def inner() -> float:
+        """Apply function to x n times."""
         nonlocal count
         count += 1
         ret = x
