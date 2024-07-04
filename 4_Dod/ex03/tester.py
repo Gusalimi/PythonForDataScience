f4ad3cd81a0d7cae98730f3cb737662726d0ab13
Guillaume Sailé <saileguillaume2@gmail.com>
2024-07-04 22:34:39 +0200

    [M04] Done

---
+++ 4_Dod/ex03/tester.py
@@ -0,0 +1,10 @@
+from new_student import Student
+
+student = Student(name="Edward", surname="agle")
+print(student)
+
+student = Student(name="Toto", surname="Titi")
+print(student)
+
+# student = Student(name="Edward", surname="agle", login="toto")
+# student = Student(name="Edward", surname="agle", id="toto")
