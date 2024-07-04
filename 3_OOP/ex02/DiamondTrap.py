f20d1008b249acb11159ce9766c0194589750502
Guillaume Sailé <saileguillaume2@gmail.com>
2024-07-04 22:36:12 +0200

    [M03][ex02] Seems ok, have to check

--- 3_OOP/ex02/DiamondTrap.py
+++ 3_OOP/ex02/DiamondTrap.py
@@ -1,20 +1,20 @@
 from S1E7 import Baratheon, Lannister
 
 
-# class King(Baratheon, Lannister):
-#     """King class, inherits from Baratheon and Lannister"""
-#     def get_eyes(self):
-#         """Return eyes color"""
-#         return self.eyes
+class King(Baratheon, Lannister):
+    """King class, inherits from Baratheon and Lannister"""
+    def get_eyes(self):
+        """Return eyes color"""
+        return self.eyes
 
-#     def get_hairs(self):
-#         """Return hairs color"""
-#         return self.hairs
+    def get_hairs(self):
+        """Return hairs color"""
+        return self.hairs
 
-#     def set_eyes(self, eyes):
-#         """Set eyes color"""
-#         self.eyes = eyes
+    def set_eyes(self, eyes):
+        """Set eyes color"""
+        self.eyes = eyes
 
-#     def set_hairs(self, hairs):
-#         """Set hairs color"""
-#         self.hairs = hairs
+    def set_hairs(self, hairs):
+        """Set hairs color"""
+        self.hairs = hairs
